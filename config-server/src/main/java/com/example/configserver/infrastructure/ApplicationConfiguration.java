aff3f5dcd49349cabefa6717b17c24f404279d04
Carlos Garcia <carlosggz@gmail.com>
2022-07-21 00:39:56 +0200

    Config server

---
+++ config-server/src/main/java/com/example/configserver/infrastructure/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.configserver.infrastructure;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class ApplicationConfiguration {
+}
